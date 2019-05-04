4aad8b9544f12bbce3cd042a6f71b54a9658bdd2
ronglai <1135332676@qq.com>
2019-05-05 00:10:52 +0800

    choosenumber bug 较多的版本

---
+++ ChooseNumber/Number.js
@@ -0,0 +1,86 @@
+//全局变量
+var x=1;
+//生成随机数
+function randomNum(minNum,maxNum){
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+            break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+            break; 
+        default: 
+            return 0; 
+        break; 
+    } 
+} 
+
+//用户选择交互
+$(document).ready(function(){
+    var $panelBody=$("#userPanel")
+    var $p1=$panelBody.children("#w1");
+    var $p2=$panelBody.children("#w2");
+    var numTwo=randomNum(-200,200);
+    var numOne=randomNum(-200,200);
+    //面板中well隐藏与显示
+    $panelBody.click(function(){       
+        if(x===1){
+            $p1.hide("slow");
+            x=x+1;
+            $p2.show("fast");
+            $("#num1").append('<h1>第一个数字是'+numOne+'</h1>');
+        }else if(x===3){
+            $panelBody.children("#w3").hide("slow");
+            $p1.show("fast")
+            numOne=randomNum(-200,200);
+            numTwo=randomNum(-200,200);
+        }
+    })
+    //生成数字和判断对错
+    $("#save").click(function(){
+        x=x+1;
+        $("#num2").append("<h1>第二个数字是"+numTwo+"</h1>");
+        $p2.hide("slow");
+        var isBig=document.getElementById("big");
+        var isSmall=document.getElementById("small");
+        if(isBig.checked){
+            if(numOne<numTwo){
+                $panelBody.append("<p id='w3'>恭喜你，你猜对了！（单击此处重新开始）</p>");
+            }else{
+                $panelBody.append("<p id='w3'>很遗憾，你猜错了。（单击此处重新开始）</p>");
+            }
+        }else if(isSmall.checked){
+            if(numOne>numTwo){
+                $panelBody.append("<p id='w3'>恭喜你，你猜对了！（单击此处重新开始）</p>");
+            }else{
+                $panelBody.append("<p id='w3'>很遗憾，你猜错了。（单击此处重新开始）</p>");
+            }
+        }else{
+            alert("请选择！")
+        }
+    })
+})
+//讲解的代码
+//有bug
+$(function(){
+    $("#saveUser").click(function(){
+        var numOne=$("#numOne").text;
+        var numTwo=$("#numTwo").text;
+        if(numOne<0.5){
+            $("#showAws").append('<h3>基准数大于第一个数，所以电脑的答案是<h1>小</h1></h3>');
+            if(numOne<numTwo){
+                $("#showAws").append('<h3>很显然，电脑答对了</h3>');
+            }else{
+                $("#showAws").append('<h3>很显然，电脑答错了</h3>');
+            }
+        }else{
+            $("#showAws").append('<h3>基准数小于第一个数，所以电脑的答案是<h1>大</h1></h3>');
+            if(numOne<numTwo){
+                $("#showAws").append('<h3>很显然，电脑答错了</h3>');
+            }else{
+                $("#showAws").append('<h3>很显然，电脑答对了</h3>');
+            }
+        }
+    })
+
+})
